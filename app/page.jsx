aca0b073bf3c4d0483fe1de321cc7bc8e51593a3
HienDinh <dnmhien3010@gmail.com>
2024-11-09 11:40:29 -0600

    Setup connection to MongoDB

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import HomeProperties from "@/components/HompeProperties";
+import connectDB from "@/config/database";
 
 const HomePage = () => {
+    connectDB();
     return (
         <>
             <Hero />
