f16e6561877d364df45869a6bd9ff84f53684bcf
HienDinh <dnmhien3010@gmail.com>
2024-11-12 07:18:14 -0600

    Single Property and Header Image

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,24 @@
-const PropertyPagae = () => {
-    return (<div>Property Page</div>);
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import Goback from "@/components/Goback";
+
+const PropertyPage = async ({ params }) => {
+    await connectDB();
+    const property = await Property.findById(params.id).lean();
+    return (
+        <>
+            <PropertyHeaderImage image={property.images[0]} />
+            <Goback />
+            <section className="bg-blue-50">
+                <div className="container m-auto py-10 px-6">
+                    <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+                        {/* Property Info */}
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 }
 
-export default PropertyPagae;
+export default PropertyPage;
