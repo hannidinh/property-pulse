e0f43ef336bd6ec399262812877af3930852b84c
HienDinh <dnmhien3010@gmail.com>
2024-11-13 07:58:12 -0600

    Property Details Component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -2,10 +2,13 @@ import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import Goback from "@/components/Goback";
+import PropertyDetails from "@/components/PropertyDetails";
 
 const PropertyPage = async ({ params }) => {
     await connectDB();
-    const property = await Property.findById(params.id).lean();
+    // Await params to access `id` properly
+    const { id } = await params;
+    const property = await Property.findById(id).lean();
     return (
         <>
             <PropertyHeaderImage image={property.images[0]} />
@@ -14,6 +17,7 @@ const PropertyPage = async ({ params }) => {
                 <div className="container m-auto py-10 px-6">
                     <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
                         {/* Property Info */}
+                        <PropertyDetails property={property} />
                     </div>
                 </div>
             </section>
