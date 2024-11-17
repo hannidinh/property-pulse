0651969025a103ba12e03e362d12ec810dab9ded
HienDinh <dnmhien3010@gmail.com>
2024-11-17 09:28:52 -0600

    Submit Property To Database
    Persist new property to database, revalidate the cached version of homepage and layout, redirect to the detail page of new property

--- actions/addProperty.js
+++ actions/addProperty.js
@@ -1,12 +1,27 @@
 'use server';
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+import { revalidatePath } from "next/cache";
+import { redirect } from 'next/navigation';
+
 
 async function addProperty(formData) {
+    await connectDB();
+    const sessionUser = await getSessionUser();
+    if (!sessionUser || !sessionUser.userId) {
+        throw new Error('User ID is required');
+    }
+
+    const { userId } = sessionUser;
+
     // Access all values from amenities and images
     const amenities = formData.getAll('amenities');
     const images = formData.getAll('images').filter((image) => image.name !== '')
         .map((image) => image.name);
 
     const propertyData = {
+        owner: userId,
         type: formData.get('type'),
         name: formData.get('name'),
         description: formData.get('description'),
@@ -32,7 +47,11 @@ async function addProperty(formData) {
         },
         images,
     };
-    console.log(propertyData);
+    const newProperty = new Property(propertyData);
+    await newProperty.save();
+
+    revalidatePath('/', 'layout');
+    redirect(`/properties/${newProperty._id}`);
 }
 
 export default addProperty;
