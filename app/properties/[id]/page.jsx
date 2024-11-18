2555c3be5b893886899bcf717c24d19eb19c05b1
HienDinh <dnmhien3010@gmail.com>
2024-11-18 06:22:59 -0600

    Serialize Lean Documents

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -4,12 +4,17 @@ import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import Goback from "@/components/Goback";
 import PropertyDetails from "@/components/PropertyDetails";
+import { convertToSerializableObject } from "@/utils/convertToObject";
 
 const PropertyPage = async ({ params }) => {
     await connectDB();
     // Await params to access `id` properly
     const { id } = await params;
-    const property = await Property.findById(id).lean();
+    const propertyDoc = await Property.findById(id).lean();
+    const property = convertToSerializableObject(propertyDoc);
+    if (!property) {
+        return <h1 className="text-center text-2xl font-bold mt-10">Property Not Found</h1>
+    }
     return (
         <>
             <PropertyHeaderImage image={property.images[0]} />
