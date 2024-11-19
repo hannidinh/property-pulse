77f12589a7e619ed75bdf9be518e8d18b0a26cec
HienDinh <dnmhien3010@gmail.com>
2024-11-18 23:33:57 -0600

    Edit Property Form

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
-import HomeProperties from "@/components/HompeProperties";
+import HomeProperties from "@/components/HomeProperties";
 import connectDB from "@/config/database";
 
 const HomePage = () => {
--- components/ProfileProperties.jsx
+++ components/ProfileProperties.jsx
@@ -34,12 +34,12 @@ const ProfileProperties = ({ properties: initialProperties }) => {
                 <p className="text-gray-600">Address: {property.location.street} {property.location.city} {property.location.state}</p>
             </div>
             <div className="mt-2">
-                <a
-                    href="/add-property.html"
+                <Link
+                    href={`/properties/${property._id}/edit`}
                     className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
                 >
                     Edit
-                </a>
+                </Link>
                 <button
                     className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
                     type="button"
